cb972aee5dcb834a23eea7740045e46520d71e18
shichengshuo <145550620+shichengshuo@users.noreply.github.com>
2024-09-23 14:12:08 +0800

    首页 详情

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import DB from "./crowdfunding_db.js";
 const app = express();
 // 处理跨域
 app.use(cors())
@@ -8,6 +9,39 @@ app.use(cors())
 app.get('/test', function (req, res) {
     res.status(200).send('test')
 })
+
+// 获取分类
+app.get("/categories", async (req, res) => {
+    try {
+        const [rows] = await DB.query("SELECT * FROM CATEGORY");
+        res.status(200).send(rows);
+    } catch (err) {
+        res.status(500);
+    }
+});
+
+
+// 筹款项目列表
+app.get("/fundraisers", async (req, res) => {
+    try {
+        const [rows] = await DB.query('SELECT * FROM FUNDRAISER');
+        res.status(200).send(rows)
+    } catch (err) {
+        res.status(500);
+    }
+});
+
+// 查询id
+app.get("/fundraiser/:id", async (req, res) => {
+    const fundraiserId = req.params.id;
+    try {
+        const [rows] = await DB.query(`SELECT * FROM FUNDRAISER WHERE FUNDRAISER_ID = ${fundraiserId}`);
+        res.status(200).send(rows[0]);
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+});
+
 // 启动服务器
 app.listen(3000, () => {
     console.log(`http://localhost:3000/`);
